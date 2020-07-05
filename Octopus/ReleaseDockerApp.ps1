206314ca4c2b6ae9f4178f3b0193ac2077b7a7c8
Gervasio Marchand <github@gervas.io>
2020-07-05 15:23:55 -0300

    Have docker-compose output everything to stdout and extract the nuget package

--- Octopus/ReleaseDockerApp.ps1
+++ Octopus/ReleaseDockerApp.ps1
@@ -1,5 +1,7 @@
 Set-PSDebug -Trace 1
 
+$OctopusParameters | ConvertTo-Json | Write-Host
+
 $path = $OctopusParameters["App.Path"]
 $packagePath = $OctopusParameters["Octopus.Action.Package[Package].ExtractedPath"]
 $dockerComposeYaml = $OctopusParameters["Yaml.Path"]
@@ -7,4 +9,7 @@ $dockerComposeYaml = $OctopusParameters["Yaml.Path"]
 rm -rf $path
 mv $packagePath $path
 
-docker-compose -f $dockerComposeYaml restart
+docker-compose -f $dockerComposeYaml restart *>&1
+if ($LASTEXITCODE) {
+    Exit $LASTEXITCODE
+}
