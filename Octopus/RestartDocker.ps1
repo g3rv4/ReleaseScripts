128e1ed492c294de7d84c91318d8e1de758ea257
Gervasio Marchand <github@gervas.io>
2020-07-13 00:42:35 -0300

    Write before running the Octopus command so that it's always in a new line

--- Octopus/RestartDocker.ps1
+++ Octopus/RestartDocker.ps1
@@ -1,5 +1,6 @@
 $dockerComposeYaml = $OctopusParameters["Yaml.Path"]
 
+Write-Host 'Restarting docker...'
 Write-Host "##octopus[stderr-progress]"
 docker-compose -f $dockerComposeYaml restart
 if ($LASTEXITCODE) {
