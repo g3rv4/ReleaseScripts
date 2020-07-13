de31979a5e2eaab7c479a3ef2db82f0625655992
Gervasio Marchand <github@gervas.io>
2020-07-13 00:50:50 -0300

    Flush!

--- Octopus/RestartDocker.ps1
+++ Octopus/RestartDocker.ps1
@@ -1,6 +1,8 @@
 $dockerComposeYaml = $OctopusParameters["Yaml.Path"]
 
 Write-Host 'Restarting docker...'
+[Console]::Out.Flush()
+
 Write-Host "##octopus[stderr-progress]"
 docker-compose -f $dockerComposeYaml restart
 if ($LASTEXITCODE) {
