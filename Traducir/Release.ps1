feba88d28047993900c62df3311ed595a1755c26
Gervasio Marchand <github@gervas.io>
2020-11-26 17:39:53 -0300

    Handle a Traducir deploy

---
+++ Traducir/Release.ps1
@@ -0,0 +1,47 @@
+$path = $OctopusParameters["App.Path"]
+$staticFilesPath = $OctopusParameters["StaticFiles.Path"]
+$dockerNginxPath = $OctopusParameters["Yaml.Nginx.Path"]
+$packagePath = $OctopusParameters["Octopus.Action.Package[Package].ExtractedPath"]
+$dockerComposeYaml = $OctopusParameters["Yaml.Path"]
+$instanceNames = $OctopusParameters["DockerInstance.Names"].Split(',')
+
+rm -rf $path
+mv $packagePath $path
+
+Write-Host "##octopus[stderr-progress]"
+
+docker-compose -f $dockerComposeYaml stop
+if ($LASTEXITCODE) {
+    Exit $LASTEXITCODE
+}
+
+rm -rf "$($path)/volumes/app"
+mv $packagePath "$($path)/volumes/app"
+
+rm -rf "$($staticFilesPath)"
+cp -r "$($path)/volumes/app/wwwroot" $staticFilesPath
+
+docker-compose -f $dockerComposeYaml up -d
+if ($LASTEXITCODE) {
+    Exit $LASTEXITCODE
+}
+
+# Give it 5 seconds for it to start
+Start-Sleep -s 5
+
+# Run migrations
+foreach ($instanceName in $instanceNames) {
+    Write-Output "Running migrations on $instanceName"
+    docker exec $instanceName wget http://localhost:5000/admin/migrate -O -
+    if ($LASTEXITCODE) {
+        Exit $LASTEXITCODE
+    }
+}
+
+# when messing with multiple containers, nginx gets confused... restarting it clears it up
+if ($instanceNames.Count -gt 1) {
+    docker-compose -f $dockerNginxPath restart
+    if ($LASTEXITCODE) {
+        Exit $LASTEXITCODE
+    }
+}
